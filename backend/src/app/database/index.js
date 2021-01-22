fe8a2b3b97d89c0a7a1680e2f6973cb86f91ca7a
emerson.diego.duarte <emerson.diego.duarte@gmail.com>
2021-01-22 20:33:38 -0300

    ajuste heroku

--- backend/src/app/database/index.js
+++ backend/src/app/database/index.js
@@ -1,13 +1,11 @@
 const {Client} = require('pg');
 
 
+
+
 const client = new Client({
-  host: process.env.DATABASE_URL || 'localhost',
-  port: 5432,
-  user: 'ssnoqgpxgsbasp',
-  password: 'ed959aad93f2c397699e2b284899746f0dbad4f5598e3203f0e244b069dc4062',
-  database: 'd27ieu3tc65cf6',
-  ssl: true
+  connectionString: process.env.DATABASE_URL,
+
 });
 
 client.connect();
